8b5df8ef5df866746e1892b482bf9bfb6f712a5e
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-02-25 16:07:27 +0700

    [JAV002]-Add method interface CategoryRepositoryServices

--- core/src/main/java/jav002/core/category/ports/CategoryRepositoryService.java
+++ core/src/main/java/jav002/core/category/ports/CategoryRepositoryService.java
@@ -1,4 +1,12 @@
 package jav002.core.category.ports;
 
-public class CategoryRepositoryService {
+import jdk.jfr.Category;
+
+import java.util.Collection;
+
+public interface CategoryRepositoryService {
+    public Collection<Category> getAllCategories();
+    public void saveCategory(Category category);
+    public Boolean doesCategoryNameExists(String name);
+
 }
