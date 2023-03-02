dc60d4cff7f7d061af7d781817a57c6a7fd84089
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 03:29:27 +0700

    [JAV-002] Create logic in interface method GetAllCategoriesUseCaseImpl

--- core/src/main/java/jav002/core/category/usecase/GetAllCategoriesUseCaseImpl.java
+++ core/src/main/java/jav002/core/category/usecase/GetAllCategoriesUseCaseImpl.java
@@ -1,4 +1,17 @@
 package jav002.core.category.usecase;
 
-public class GetAllCategoriesUseCaseImpl {
+import jav002.core.category.ports.CategoryRepositoryService;
+import jav002.core.category.Category
+import lombok.AllArgsConstructor;
+
+import java.util.Collection;
+
+@AllArgsConstructor
+public class GetAllCategoriesUseCaseImpl implements GetAllCategoriesUseCase{
+    private final CategoryRepositoryService categoryRepositoryService;
+
+    @Override
+    public Collection<Category> execute() {
+        return categoryRepositoryService.getAllCategories();
+    }
 }
