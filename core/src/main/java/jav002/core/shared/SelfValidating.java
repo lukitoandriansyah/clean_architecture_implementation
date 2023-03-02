0b4dd1e6695c1f8e1e1836dfe314825bbef00a5a
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 04:15:08 +0700

    [JAV-002] Create logic in CLASSe method SelfValidating

--- core/src/main/java/jav002/core/shared/SelfValidating.java
+++ core/src/main/java/jav002/core/shared/SelfValidating.java
@@ -1,4 +1,18 @@
 package jav002.core.shared;
 
-public class SelfValidating {
+import javax.validation.*;
+import java.util.Set;
+
+public abstract class SelfValidating<T> {
+    private Validator validator;
+    protected SelfValidating(){
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+    protected void validatorSelf(){
+        Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+        if (violations.isEmpty()){
+            throw new ConstraintViolationException(violations);
+        }
+    }
 }
