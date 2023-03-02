e663168e9e1641f895509643fe30db0e79119653
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 04:07:52 +0700

    [JAV-002] Create logic in CLASSe method Category and Add some library at Pom.xml

--- core/src/main/java/jav002/core/category/Category.java
+++ core/src/main/java/jav002/core/category/Category.java
@@ -1,4 +1,19 @@
 package jav002.core.category;
 
-public class Category {
+import jav002.core.shared.SelfValidating;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class Category extends SelfValidating<Category> implements Serializable {
+
+    @Min(0)
+    private Long id;
+    @NotEmpty
+    private String name;
+    private Boolean available;
 }
