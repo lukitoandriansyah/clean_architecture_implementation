dc8b0849a58932eff9e076b7ecbb3936ce939335
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 18:04:54 +0700

    [JAV-002] Add logic in class CategoryRepositoryConverter

--- infrastructure/src/main/java/jav002/infrastructure/persistence/converters/CategoryRepositoryConverter.java
+++ infrastructure/src/main/java/jav002/infrastructure/persistence/converters/CategoryRepositoryConverter.java
@@ -1,4 +1,16 @@
 package jav002.infrastructure.persistence.converters;
 
-public class CategoryRepositoryConverter {
+import jav002.core.category.Category;
+import jav002.infrastructure.persistence.entities.CategoryEntity;
+import jav002.infrastructure.shared.RepositoryConverter;
+
+public class CategoryRepositoryConverter implements RepositoryConverter<CategoryEntity, Category> {
+    @Override
+    public CategoryEntity mapToTable(final Category persistenceObject){
+        return new CategoryEntity(persistenceObject.getId(), persistenceObject.getName(), persistenceObject.getAvailable());
+    }
+    @Override
+    public Category mapToEntity(final CategoryEntity entityObject){
+        return new Category(entityObject.getId(), entityObject.getName(), entityObject.getAvailable());
+    }
 }
