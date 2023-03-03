b8f1cd4d6fd44652c4b25d4ff3b45868c33dfd46
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 19:49:09 +0700

    [JAV-002] Add logic in class CommonConstants

--- infrastructure/src/main/java/jav002/infrastructure/shared/constants/CommonConstants.java
+++ infrastructure/src/main/java/jav002/infrastructure/shared/constants/CommonConstants.java
@@ -1,4 +1,9 @@
 package jav002.infrastructure.shared.constants;
 
 public class CommonConstants {
+    public static  final String SUCCESS = "Success";
+    public static final String OK = "OK";
+    public static final String UTF_8="UTF-8";
+
+    private CommonConstants(){throw new IllegalAccessException("Utility Class");}
 }
