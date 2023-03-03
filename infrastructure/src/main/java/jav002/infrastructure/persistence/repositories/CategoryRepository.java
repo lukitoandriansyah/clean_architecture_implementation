9cebbadc4762c6f2ac3e79f446bae88085573c69
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 18:30:33 +0700

    [JAV-002] Add logic in interface CategoryRepository

--- infrastructure/src/main/java/jav002/infrastructure/persistence/repositories/CategoryRepository.java
+++ infrastructure/src/main/java/jav002/infrastructure/persistence/repositories/CategoryRepository.java
@@ -1,4 +1,11 @@
 package jav002.infrastructure.persistence.repositories;
 
-public class CategoryRepository {
+import jav002.core.category.Category;
+import jav002.infrastructure.persistence.entities.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+    public Collection<Category> findByName(String name);
 }
