0e9540220881203878ade20eccd2113b19c37eee
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 19:54:50 +0700

    [JAV-002] Add logic in class RestConstants

--- infrastructure/src/main/java/jav002/infrastructure/shared/constants/RestConstants.java
+++ infrastructure/src/main/java/jav002/infrastructure/shared/constants/RestConstants.java
@@ -1,4 +1,7 @@
 package jav002.infrastructure.shared.constants;
 
 public class RestConstants {
+    public static final String APPLICATION_NAME = "/netflix";
+    public static final String API_VERSION = "/v1";
+    public static final String RESOURCE_CATEGORY = "/category";
 }
