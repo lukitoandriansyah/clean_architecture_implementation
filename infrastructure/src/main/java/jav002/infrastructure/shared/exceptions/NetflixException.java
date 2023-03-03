b97a517575156957eecf4b7991002ef6edd8f564
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 20:00:44 +0700

    [JAV-002] Add logic in class NetflixException

--- infrastructure/src/main/java/jav002/infrastructure/shared/exceptions/NetflixException.java
+++ infrastructure/src/main/java/jav002/infrastructure/shared/exceptions/NetflixException.java
@@ -1,4 +1,15 @@
 package jav002.infrastructure.shared.exceptions;
 
-public class NetflixException {
+public abstract class NetflixException extends Exception {
+    private static final longserialVersionUID = -7482635401716007171L;
+    private final int code;
+
+    public NetflixException(final int code, final String message){
+        super(message);
+        this.code=code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
 }
