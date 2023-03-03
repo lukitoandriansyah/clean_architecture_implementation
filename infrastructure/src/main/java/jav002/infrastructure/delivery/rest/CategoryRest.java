d81ea1cf2b2b4978006229ae262c05e421c2599e
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 17:49:33 +0700

    [JAV-002] Add logic in class CategoryRest

--- infrastructure/src/main/java/jav002/infrastructure/delivery/rest/CategoryRest.java
+++ infrastructure/src/main/java/jav002/infrastructure/delivery/rest/CategoryRest.java
@@ -1,4 +1,20 @@
 package jav002.infrastructure.delivery.rest;
 
-public class CategoryRest {
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryRest implements Serializable {
+    private static final long serialVersionUID = 7809559376441998463L;
+
+    @NotNull
+    private String name;
+    @NotNull
+    private Boolean available;
 }
