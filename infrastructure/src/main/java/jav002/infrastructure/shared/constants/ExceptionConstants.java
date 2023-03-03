85643c2ff6568cc035bb179da78524d9f7ed277d
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 19:52:47 +0700

    [JAV-002] Add logic in class ExceptionConstants

--- infrastructure/src/main/java/jav002/infrastructure/shared/constants/ExceptionConstants.java
+++ infrastructure/src/main/java/jav002/infrastructure/shared/constants/ExceptionConstants.java
@@ -1,4 +1,10 @@
 package jav002.infrastructure.shared.constants;
 
 public class ExceptionConstants {
+    public static final String ERROR="ERROR";
+    public static final String BAD_REQUEST_DEFAULT_MESSAGE = "There are unaccepted parameters";
+    public static final String BAD_REQUEST_EXISTS_CATEGORY_MESSAGE = "This category already exists";
+    private ExceptionConstants(){
+        throw new IllegalStateException("Utility Class");
+    }
 }
