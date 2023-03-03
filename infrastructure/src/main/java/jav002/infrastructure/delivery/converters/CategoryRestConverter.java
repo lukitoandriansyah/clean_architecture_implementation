c0cdeaf07c03c1feeb95773ec4208cda206837f3
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 17:17:20 +0700

    [JAV-002] Add logic in class CategoryRestConverter

--- infrastructure/src/main/java/jav002/infrastructure/delivery/converters/CategoryRestConverter.java
+++ infrastructure/src/main/java/jav002/infrastructure/delivery/converters/CategoryRestConverter.java
@@ -1,4 +1,17 @@
 package jav002.infrastructure.delivery.converters;
 
-public class CategoryRestConverter {
+import jav002.core.category.Category;
+import jav002.infrastructure.delivery.rest.CategoryRest;
+import jav002.infrastructure.shared.RestConverter;
+
+public class CategoryRestConverter implements RestConverter<CategoryRest, Category> {
+    @Override
+    public Category mapToEntity(final CategoryRest categoryRest){
+        return new Category(null, categoryRest.getName(), categoryRest.getAvailable());
+    }
+
+    @Override
+    public CategoryRest mapToRest(final Category category){
+        return new CategoryRest(category.getName(), category.getAvailable());
+    }
 }
