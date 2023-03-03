19da7140518d5e04bc4907a82bbc95231ae7a4cc
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 20:02:57 +0700

    [JAV-002] Add logic in class NotFoundException

--- infrastructure/src/main/java/jav002/infrastructure/shared/exceptions/NotFoundException.java
+++ infrastructure/src/main/java/jav002/infrastructure/shared/exceptions/NotFoundException.java
@@ -1,4 +1,11 @@
 package jav002.infrastructure.shared.exceptions;
 
-public class NotFoundException {
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends NetflixException{
+    private static final long serialVersionUID = -6870732210014274010L;
+
+    public NotFoundException(final String message){
+        super(HttpStatus.NOT_FOUND.value(), message);
+    }
 }
