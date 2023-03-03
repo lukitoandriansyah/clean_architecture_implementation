932f533c5cb72c3a4514bd01294f289ba3e446d3
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 18:13:47 +0700

    [JAV-002] Add logic in class CategoryEntity

--- infrastructure/src/main/java/jav002/infrastructure/persistence/entities/CategoryEntity.java
+++ infrastructure/src/main/java/jav002/infrastructure/persistence/entities/CategoryEntity.java
@@ -1,4 +1,25 @@
 package jav002.infrastructure.persistence.entities;
 
-public class CategoryEntity {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@Entity
+@Table(name = "CATEGORY")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryEntity implements Serializable {
+    private static final long serialVersionUID=4471777119419172870L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categorySeq")
+    @SequenceGenerator(name = "categorySeq", sequenceName = "category_id_Seq", allocationSize = 1)
+    private Long id;
+    @Column(name = "NAME", unique = true)
+    private String name;
+    @Column(name = "AVAILABLE", unique = true)
+    private Boolean available;
+
 }
