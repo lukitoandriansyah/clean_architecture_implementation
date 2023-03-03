c73337d9684c44767ef8cd18660b23410bd78251
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 18:28:13 +0700

    [JAV-002] Add logic in class CategoryServiceImpl

--- infrastructure/src/main/java/jav002/infrastructure/persistence/impl/CategoryServiceImpl.java
+++ infrastructure/src/main/java/jav002/infrastructure/persistence/impl/CategoryServiceImpl.java
@@ -1,4 +1,33 @@
 package jav002.infrastructure.persistence.impl;
 
-public class CategoryServiceImpl {
+import jav002.core.category.ports.CategoryRepositoryService;
+import jav002.infrastructure.persistence.converters.CategoryRepositoryConverter;
+import jav002.infrastructure.persistence.repositories.CategoryRepository;
+import jav002.core.category.Category;
+import lombok.AllArgsConstructor;
+
+import java.util.Collection;
+import java.util.stream.Collector;
+
+@AllArgsConstructor
+public class CategoryServiceImpl implements CategoryRepositoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryRepositoryConverter categoryRepositoryConverter;
+
+    @Override
+    public Collection<Category> getAllCategories() {
+        return categoryRepository.findAll().stream().map(category->categoryRepositoryConverter.mapToEntity(category))
+                .collect(Collector.toList());
+    }
+
+    @Override
+    public void saveCategory(Category category) {
+        categoryRepository.save(categoryRepositoryConverter.mapToTable(category));
+    }
+
+    @Override
+    public Boolean doesCategoryNameExists(String name) {
+        return !categoryRepository.findByName(name).isEmpty();
+    }
 }
