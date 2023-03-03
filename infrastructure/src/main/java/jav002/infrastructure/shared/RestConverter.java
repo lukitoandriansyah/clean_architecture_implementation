350c3263af2e04a264addcd5a7f6a8e8726d3db3
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-03-03 20:08:35 +0700

    [JAV-002] Add logic in interface RestConverter

--- infrastructure/src/main/java/jav002/infrastructure/shared/RestConverter.java
+++ infrastructure/src/main/java/jav002/infrastructure/shared/RestConverter.java
@@ -1,4 +1,12 @@
 package jav002.infrastructure.shared;
 
-public class RestConverter {
+import java.io.Serializable;
+
+public interface RestConverter <R extends Serializable, E extends Serializable>{
+    default E mapToEntity(final R rest){
+        throw new UnsupportedOperationException();
+    }
+    default R mapToRest(final E entity){
+        throw new UnsupportedOperationException();
+    }
 }
